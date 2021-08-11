dd0d4ed7784ef9d8c3bbdb2ccbe185c8d6ea3ed3
Linh Nguyen <linh1610it@gmail.com>
2021-08-11 11:43:25 +0700

    init

---
+++ content/sites/deploy-now/index.md
@@ -0,0 +1,21 @@
+---
+title: Deploy Now
+homepage: https://deploynow.space
+description: Deploy smarter. Deploy faster. Deploy Now.
+twitter: ionos_com
+freeplan: true
+autodeployment: true
+pricing: https://deploynow.space
+---
+
+Deploy Now is instant hosting from your GitHub repository. - Just edit, push, and your changes are live.
+
+Every project comes with ...
+
+- Auto-build setup support
+- Continuous and automatic deploys from GitHub
+- Staging of your branches
+- Custom domain support
+- Automatic SSL certificate issuance and renewal
+- Automatic HTTP → HTTPS redirects
+- And more …
---
+++ src/components/seo.js
@@ -0,0 +1,94 @@
+/**
+ * SEO component that queries for data with
+ *  Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
+
+const SEO = ({ description, lang, meta, title }) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `
+  )
+
+  const metaDescription = description || site.siteMetadata.description
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={
+        title
+          ? `${title} | ${site.siteMetadata.title}`
+          : `${site.siteMetadata.title}`
+      }
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: `${title ? title + '| ' : ''}${site.siteMetadata.title}`,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          property: `og:image`,
+          content: `https://cdn.cosmicjs.com/199f4880-c7a2-11ea-b44f-f5c7da208e23-staticwebsitehosting.png`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: "@cosmicjs",
+        },
+        {
+          name: `twitter:title`,
+          content: `${title ? title + '| ' : ''}${site.siteMetadata.title}`,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ].concat(meta)}
+    />
+  )
+}
+
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  description: ``,
+}
+
+SEO.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string,
+}
+
+export default SEO
